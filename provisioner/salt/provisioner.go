eeb2f08c5f8cc92fbe15e29b0ef24cb970c1257a
Michael Poore <michael@michaelpoore.com>
2024-06-11 15:09:56 +0100

    Trying new PS command

--- provisioner/salt/provisioner.go
+++ provisioner/salt/provisioner.go
@@ -233,7 +233,7 @@ func (p *Provisioner) createDir(ui packersdk.Ui, comm packersdk.Communicator, di
 	ctx := context.TODO()
 	command := fmt.Sprintf("mkdir -p '%s'", dir)
 	if p.config.IsWindows {
-		command = fmt.Sprintf("New-Item -ItemType Directory -Path %s -Force", dir)
+		command = fmt.Sprintf("PowerShell -ExecutionPolicy Bypass -OutputFormat Text -Command 'New-Item -ItemType Directory -Path %s -Force'", dir)
 	}
 	cmd := &packersdk.RemoteCmd{
 		Command: command,
